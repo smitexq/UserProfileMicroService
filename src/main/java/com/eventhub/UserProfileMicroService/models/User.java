be47cc41ad79ce39f8f28bb8f215d2df6974a251
smitexq <spygidak@gmail.com>
2025-06-25 12:24:39 +0300

    first commit

---
+++ src/main/java/com/eventhub/UserProfileMicroService/models/User.java
@@ -0,0 +1,58 @@
+package com.eventhub.UserProfileMicroService.models;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+
+
+    public User() {}
+    public User(String username, String email, String password) {
+        setUsername(username);
+        setEmail(email);
+        setPassword(password);
+    }
+
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/com/eventhub/UserProfileMicroService/UserProfileMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventhub.UserProfileMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
