be599379b9caad009330aca7cb719208c42b080a
smitexq <spygidak@gmail.com>
2025-09-22 19:22:07 +0300

    в репозиториях запросы теперь делаются на чистом sql через @query

--- src/main/java/com/eventhub/UserProfileMicroService/dao/EventRepository.java
+++ src/main/java/com/eventhub/UserProfileMicroService/dao/EventRepository.java
@@ -2,11 +2,18 @@ package com.eventhub.UserProfileMicroService.dao;
 
 import com.eventhub.UserProfileMicroService.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-    Optional<Event> findByName(String name);
+
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM event AS e WHERE e.name = :name"
+    )
+    Optional<Event> findByName(@Param("name") String name);
 }
--- src/main/java/com/eventhub/UserProfileMicroService/dao/ProfileRepository.java
+++ src/main/java/com/eventhub/UserProfileMicroService/dao/ProfileRepository.java
@@ -2,6 +2,8 @@ package com.eventhub.UserProfileMicroService.dao;
 
 import com.eventhub.UserProfileMicroService.models.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,10 @@ import java.util.UUID;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, UUID> {
-    Optional<Profile> findByUsername(String username);
+
+    @Query(
+       nativeQuery = true,
+       value = "SELECT * FROM profile AS p WHERE p.username = :username"
+    )
+    Optional<Profile> findByUsername(@Param("username") String username);
 }
